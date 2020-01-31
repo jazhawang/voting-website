1b8316f804c5c3a1b1c570d8c0fb636e99f1d657
LEFTA98 <sqshyzerg353@gmail.com>
2020-01-31 14:53:05 -0500

    add utf8 encoding to main

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import Network.Wai.Handler.Warp
+import GHC.IO.Encoding
 import Lib
 
 main :: IO ()
 main = do
+    setLocaleEncoding utf8
     let port = 8080
     putStrLn $ "✅  Running app on port: " ++ show port
     run port app
